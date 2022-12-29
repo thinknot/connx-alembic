2736cae0e5a160c1214a90f51dbb42c892589ae0
Caleb <caleb@ipoweralliance.com>
2022-12-29 10:55:33 -0700

    definitely fix that

--- src/flapp/extension.py
+++ src/flapp/extension.py
@@ -4,5 +4,5 @@ from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 
 logging.debug("Constructing flask extensions...")
-ma = Marshmallow()
 db = SQLAlchemy()
+ma = Marshmallow()
